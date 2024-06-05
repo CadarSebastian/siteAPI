0882cf017d6352c4493aba08c3c59c593bd41254
CadarSebastian <sebicadar03@gmail.com>
2024-06-05 22:07:18 +0300

    db change

--- db-sync.js
+++ db-sync.js
@@ -3,7 +3,7 @@ import { sequelize } from "./db.js";
 import { Copii } from "./models/copii.model.js";
 import { Sesiuni } from "./models/sesiuni.model.js";
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
   console.log("FINISHED SUCCESS");
   process.exit(0);
 });
